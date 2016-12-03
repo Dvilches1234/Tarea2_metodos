f7edb303da9bcd8e54bea151136a9355e9eceb47
javiaraya <x.chika19@gmail.com>
2016-12-03 19:13:25 -0300

    Add files via upload

---
+++ Funciones/trapecio.m
@@ -0,0 +1,26 @@
+z=1:7;
+w=[122,130,135,160,175,190,200];
+n=length(w);
+
+
+h=(w(7)-w(1));
+f(z)=(10^3)*(9.8)*w(z).*(6-z).*10; 
+%f(z) de 1 a 7
+%para ocupar el f real de un numero ocupar faux(numero,f)
+%con f(0) te va tirar error, con faux(0,f) 
+s=0;
+
+
+for i=2:n-1 
+    
+    s=s+f(i);
+
+end
+trap=(h/2)*(w(1)+2*s+w(7))
+
+plot(w,f,'m')
+hold on
+plot(w,f,'g*')
+xlabel('w')
+ylabel('f')
+
