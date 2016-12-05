acaf704cb883a6f4274c4e3a287dda1bf141eab0
Thomas Munoz <thomas.fmunoz@gmail.com>
2016-12-05 01:10:30 -0300

    Graficador de 4 b

---
+++ Funciones/graph.m
@@ -0,0 +1,31 @@
+m=50;
+n=m;
+
+t=1:m;
+for i=1 : m
+	t(i)=-1+(i-1)*(2/(m-1));
+end
+
+f=ones(m);
+f=f(:,1);
+for i=1 : m
+	f(i)=1/(1+(25*(t(i)^2)));
+end
+
+a=ones(m);
+for i=2 : m
+	for j=2 : n
+		a(i,j)=t(i-1)^(j-1);
+	end
+end
+
+x=Eliminacion_gaussiana(a,f)
+
+g=1:m;
+for i=1 : m
+	g(i)=x(i)*t(i)^(i-1)
+end
+
+plot(t, g);
+hold on; % without this one will delete y1 before drawing y2
+plot(t, f, 'r');
